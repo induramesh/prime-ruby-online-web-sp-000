c71bb3bd4b19332d69f8ed2166c1d895f82f6be8
Indu Ramesh <induramesh@gmail.com>
2019-07-24 19:16:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,14 +2,15 @@
 
 def prime?(num)
   if num <= 1
-    false 
+    return false 
   else 
   #Check from 2 to sqrt(num-1)
   iter = 2 
-  while iter < num 
+  while iter < sqrt(num)
     if n % i == 0 
-      false 
-    
-    true 
+      return false 
+    i++
+  end 
+    return true 
   end 
 end 
